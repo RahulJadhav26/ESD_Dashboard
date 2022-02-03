d122e11f59b7d76e2e0e49bf8638687d4af16218
RahulJadhav2611 <rrahuljadhav.rj@gmail.com>
2022-02-03 18:52:50 -0500

    on premise server api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: 'http://35.172.171.121:8888' //  Prod
-    baseURL: 'http://localhost:8888' // Test
+    // baseURL: 'http://35.172.171.121:8888' //  cloud Prod
+    // baseURL: 'http://localhost:8888' // Test
+    baseURL: 'http://128.230.247.39:8888' // CENT Server
+
   })
 }
